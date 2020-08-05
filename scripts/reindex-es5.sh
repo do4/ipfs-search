6656a4f1bb505c1002d64f8224711f9ebc1b4343
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-08-05 18:32:29 +0100

    Full reindex commands.

--- scripts/reindex-es5.sh
+++ scripts/reindex-es5.sh
@@ -3,34 +3,295 @@
 # Note: this script assumes that the target indexes exist (ipfs_files_v0, ipfs_directories_v0, ipfs_invalids_v0)
 # Note: for now, this is a pseudo-script
 
-# Start crawler, create new indexes, test crawling
-
-# Disable crawler
-
 # Register snapshot repository
+PUT /_snapshot/ipfs_s3_v0
+{
+  "type": "s3",
+  "settings": {
+    "bucket": "ipfs-search-snapshots",
+    "storage_class": "standard_ia",
+    "compress": true,
+    "role_arn": "arn:aws:iam::123456789012:role/TheSnapshotRole"
+  }
+}
+
+# Register output snapshot repository
+PUT /_snapshot/ipfs_s3_v1
+{
+  "type": "s3",
+  "settings": {
+    "bucket": "ipfs-search-snapshots-v1",
+    "storage_class": "standard_ia",
+    "compress": true,
+    "role_arn": "arn:aws:iam::123456789012:role/TheSnapshotRole"
+  }
+}
 
 # Restore snapshot
+POST /_snapshot/ipfs_s3_v0/$SRC_SNAP/_restore
 
-# Set refresh_interval=-1 and number_of_replicas=0 for efficient reindexing.
-PUT /ipfs_invalids_v0/_settings
+# Invalids index
+PUT /ipfs_invalids_v0
 {
-  "index": {
-    "refresh_interval": -1,
-    "number_of_replicas": 0
+  "settings": {
+    "index": {
+      "refresh_interval": "-1",
+      "mapping": {
+          "total_fields": {
+              "limit": "8192"
+          }
+      },
+    "number_of_shards" : "5",
+    "number_of_replicas": "0"
+    }
+  },
+  "mappings": {
+    "properties": {
+      "_doc": {
+          "dynamic_templates": [
+              {
+                  "default_noindex": {
+                      "match": "*",
+                      "mapping": {
+                          "index": "no",
+                          "doc_values": false,
+                          "include_in_all": false,
+                          "norms": false
+                      }
+                  }
+              }
+          ],
+          "properties": {
+             "error": {
+                "type": "text",
+                "index": false
+             }
+          }
+      }
+    }
   }
 }
-PUT /ipfs_directories_v0/_settings
+
+# Directories index
+PUT /ipfs_directories_v0
 {
-  "index": {
-    "refresh_interval": -1,
-    "number_of_replicas": 0
+  "settings": {
+    "index": {
+      "refresh_interval": "-1",
+      "mapping": {
+          "total_fields": {
+              "limit": "8192"
+          }
+      },
+    "number_of_shards" : "5",
+    "number_of_replicas": "0"
+    }
+  },
+  "mappings": {
+    "properties": {
+      "_doc": {
+          "dynamic": "strict",
+          "properties": {
+              "first-seen": {
+                  "type": "date",
+                  "format": "date_time_no_millis"
+              },
+              "last-seen": {
+                  "type": "date",
+                  "format": "date_time_no_millis"
+              },
+              "links":  {
+                  "dynamic":  true,
+                  "properties": {
+                      "Hash": {
+                          "type": "keyword",
+                          "index": true
+                      },
+                      "Name": {
+                          "type": "text"
+                      },
+                      "Size": {
+                         "type": "long",
+                         "ignore_malformed": true
+                      },
+                      "Type": {
+                         "type": "keyword"
+                      }
+                   }
+              },
+              "size": {
+                  "type": "long",
+                  "ignore_malformed": true
+              },
+              "references":  {
+                  "properties": {
+                      "name": {
+                          "type": "text",
+                          "index": true
+                      },
+                      "hash": {
+                          "type": "keyword",
+                          "index": true
+                      },
+                      "parent_hash": {
+                          "type": "keyword",
+                          "index": true
+                      }
+                  }
+              }
+          }
+      }
+    }
   }
 }
-PUT /ipfs_files_v0/_settings
+
+# Files index
+PUT /ipfs_files_v0
 {
-  "index": {
-    "refresh_interval": -1,
-    "number_of_replicas": 0
+  "settings": {
+    "index": {
+      "refresh_interval": "-1",
+      "mapping": {
+          "total_fields": {
+              "limit": "8192"
+          }
+      },
+    "number_of_shards" : "5",
+    "number_of_replicas": "0"
+    }
+  },
+  "mappings": {
+    "properties": {
+     "_doc": {
+        "dynamic": "strict",
+        "dynamic_templates": [
+            {
+                "default_noindex": {
+                    "match": "*",
+                    "mapping": {
+                        "index": false,
+                        "doc_values": false,
+                  "norms": false
+                    }
+                }
+            }
+        ],
+        "properties": {
+            "first-seen": {
+                "type": "date",
+                "format": "date_time_no_millis"
+            },
+            "last-seen": {
+                "type": "date",
+                "format": "date_time_no_millis"
+            },
+            "content":  {
+                "type": "text"
+            },
+            "ipfs_tika_version": {
+              "type": "keyword"
+            },
+            "language": {
+              "properties": {
+                "confidence": {
+                  "type": "keyword"
+                },
+                "language": {
+                  "type": "keyword"
+                },
+                "rawScore": {
+                  "type": "double"
+                }
+              }
+            },
+            "metadata":  {
+                "dynamic":  "true",
+                "properties": {
+                    "title" : {
+                        "type": "text"
+                    },
+                    "name": {
+                        "type": "text"
+                    },
+                    "author": {
+                        "type": "text"
+                    },
+                    "description": {
+                        "type": "text"
+                    },
+                    "producer": {
+                        "type": "text"
+                    },
+                    "publisher": {
+                        "type": "text"
+                    },
+                    "isbn": {
+                        "type": "keyword"
+                    },
+                    "language": {
+                        "type": "keyword"
+                    },
+                    "resourceName": {
+                      "type": "keyword"
+                    },
+                    "keywords": {
+                       "type": "text"
+                    },
+                    "xmpDM:album": {
+                        "type": "text"
+                    },
+                    "xmpDM:albumArtist": {
+                        "type": "text"
+                    },
+                    "xmpDM:artist": {
+                        "type": "text"
+                    },
+                    "xmpDM:composer": {
+                        "type": "text"
+                    },
+                    "Content-Type": {
+                        "type": "keyword"
+                    },
+                    "X-Parsed-By": {
+                        "type": "keyword"
+                    },
+                    "created": {
+                        "type": "date",
+                        "format": "date_optional_time"
+                    },
+                    "date": {
+                        "type": "date",
+                        "format": "date_optional_time"
+                    },
+                    "modified": {
+                        "type": "date",
+                        "format": "date_optional_time"
+                    }
+                }
+            },
+            "urls": {
+                "type": "keyword"
+            },
+            "size": {
+                "type": "long",
+                "ignore_malformed": true
+            },
+            "references":  {
+                "properties": {
+                    "name": {
+                        "type": "text"
+                    },
+                    "hash": {
+                        "type": "keyword"
+                    },
+                    "parent_hash": {
+                        "type": "keyword"
+                    }
+                }
+            }
+        }
+      }
+    }
   }
 }
 
@@ -73,3 +334,8 @@ POST _reindex
   }
 }
 
+# Make snapshot of new indexes
+PUT /_snapshot/ipfs_s3_v1/200729-1910
+{
+  "indices": "ipfs_invalids_v0,ipfs_directories_v0,ipfs_files_v0"
+}
