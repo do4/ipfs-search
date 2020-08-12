6aa071844f678acfce54ffaab862f6d6ae2ef5c3
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-08-12 16:07:27 +0100

    Fix index configuration.

--- scripts/reindex-es5.sh
+++ scripts/reindex-es5.sh
@@ -31,7 +31,7 @@ PUT /_snapshot/ipfs_s3_v1
 POST /_snapshot/ipfs_s3_v0/$SRC_SNAP/_restore
 
 # Invalids index
-PUT /ipfs_invalids_v0
+PUT /ipfs_invalids_v6
 {
   "settings": {
     "index": {
@@ -47,33 +47,31 @@ PUT /ipfs_invalids_v0
   },
   "mappings": {
     "properties": {
-      "_doc": {
-          "dynamic_templates": [
-              {
-                  "default_noindex": {
-                      "match": "*",
-                      "mapping": {
-                          "index": "no",
-                          "doc_values": false,
-                          "include_in_all": false,
-                          "norms": false
-                      }
-                  }
-              }
-          ],
-          "properties": {
-             "error": {
-                "type": "text",
-                "index": false
-             }
-          }
-      }
+        "dynamic_templates": [
+            {
+                "default_noindex": {
+                    "match": "*",
+                    "mapping": {
+                        "index": "no",
+                        "doc_values": false,
+                        "include_in_all": false,
+                        "norms": false
+                    }
+                }
+            }
+        ],
+        "properties": {
+           "error": {
+              "type": "text",
+              "index": false
+           }
+        }
     }
   }
 }
 
 # Directories index
-PUT /ipfs_directories_v0
+PUT /ipfs_directories_v6
 {
   "settings": {
     "index": {
@@ -89,7 +87,6 @@ PUT /ipfs_directories_v0
   },
   "mappings": {
     "properties": {
-      "_doc": {
           "dynamic": "strict",
           "properties": {
               "first-seen": {
@@ -141,189 +138,187 @@ PUT /ipfs_directories_v0
               }
           }
       }
-    }
   }
 }
 
 # Files index
-PUT /ipfs_files_v0
+PUT /ipfs_files_v6
 {
   "settings": {
     "index": {
       "refresh_interval": "-1",
       "mapping": {
-          "total_fields": {
-              "limit": "8192"
-          }
+        "total_fields": {
+          "limit": "8192"
+        }
       },
-        "analysis": {
-            "filter": {
-                "shingle_filter": {
-                    "type": "shingle",
-                    "min_shingle_size": 5,
-                    "max_shingle_size": 5,
-                    "output_unigrams": false
-                },
-                "minhash_filter": {
-                    "type": "min_hash",
-                    "hash_count": 1,
-                    "bucket_count": 512,
-                    "hash_set_size": 1,
-                    "with_rotation": true
-                }
-            }
+      "analysis": {
+        "filter": {
+          "shingle_filter": {
+            "type": "shingle",
+            "min_shingle_size": 5,
+            "max_shingle_size": 5,
+            "output_unigrams": false
+          },
+          "minhash_filter": {
+            "type": "min_hash",
+            "hash_count": 1,
+            "bucket_count": 512,
+            "hash_set_size": 1,
+            "with_rotation": true
+          }
         },
         "analyzer": {
-            "fingerprint_analyzer": {
-                "tokenizer": "standard",
-                "filter": [
-                  "shingle_filter",
-                  "minhash_filter"
-                ]
-            }
+          "fingerprint_analyzer": {
+            "tokenizer": "standard",
+            "filter": [
+              "shingle_filter",
+              "minhash_filter"
+            ]
+          }
         }
-    "number_of_shards" : "5",
+      }
+    },
+    "number_of_shards": "5",
     "number_of_replicas": "0"
-    }
-  },
-  "mappings": {
+  }
+},
+"mappings": {
+  "properties": {
+    "dynamic": "strict",
+    "dynamic_templates": [
+      {
+        "default_noindex": {
+          "match": "*",
+          "mapping": {
+            "index": false,
+            "doc_values": false,
+            "norms": false
+          }
+        }
+      }
+    ],
     "properties": {
-     "_doc": {
-        "dynamic": "strict",
-        "dynamic_templates": [
-            {
-                "default_noindex": {
-                    "match": "*",
-                    "mapping": {
-                        "index": false,
-                        "doc_values": false,
-                  "norms": false
-                    }
-                }
-            }
-        ],
+      "first-seen": {
+        "type": "date",
+        "format": "date_time_no_millis"
+      },
+      "last-seen": {
+        "type": "date",
+        "format": "date_time_no_millis"
+      },
+      "content": {
+        "type": "text",
+        "term_vector": "with_positions_offsets"
+      },
+      "fingerprint": {
+        "type": "text",
+        "analyzer": "fingerprint_analyzer",
+      },
+      "ipfs_tika_version": {
+        "type": "keyword"
+      },
+      "language": {
         "properties": {
-            "first-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "last-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "content":  {
-                "type": "text",
-                "term_vector": "with_positions_offsets"
-            },
-            "fingerprint": {
-                "type": "text",
-                "analyzer": "fingerprint_analyzer",
-            },
-            "ipfs_tika_version": {
-              "type": "keyword"
-            },
-            "language": {
-              "properties": {
-                "confidence": {
-                  "type": "keyword"
-                },
-                "language": {
-                  "type": "keyword"
-                },
-                "rawScore": {
-                  "type": "double"
-                }
-              }
-            },
-            "metadata":  {
-                "dynamic":  "true",
-                "properties": {
-                    "title" : {
-                        "type": "text"
-                    },
-                    "name": {
-                        "type": "text"
-                    },
-                    "author": {
-                        "type": "text"
-                    },
-                    "description": {
-                        "type": "text"
-                    },
-                    "producer": {
-                        "type": "text"
-                    },
-                    "publisher": {
-                        "type": "text"
-                    },
-                    "isbn": {
-                        "type": "keyword"
-                    },
-                    "language": {
-                        "type": "keyword"
-                    },
-                    "resourceName": {
-                      "type": "keyword"
-                    },
-                    "keywords": {
-                       "type": "text"
-                    },
-                    "xmpDM:album": {
-                        "type": "text"
-                    },
-                    "xmpDM:albumArtist": {
-                        "type": "text"
-                    },
-                    "xmpDM:artist": {
-                        "type": "text"
-                    },
-                    "xmpDM:composer": {
-                        "type": "text"
-                    },
-                    "Content-Type": {
-                        "type": "keyword"
-                    },
-                    "X-Parsed-By": {
-                        "type": "keyword"
-                    },
-                    "created": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    },
-                    "date": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    },
-                    "modified": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    }
-                }
-            },
-            "urls": {
-                "type": "keyword"
-            },
-            "size": {
-                "type": "long",
-                "ignore_malformed": true
-            },
-            "references":  {
-                "properties": {
-                    "name": {
-                        "type": "text"
-                    },
-                    "hash": {
-                        "type": "keyword"
-                    },
-                    "parent_hash": {
-                        "type": "keyword"
-                    }
-                }
-            }
+          "confidence": {
+            "type": "keyword"
+          },
+          "language": {
+            "type": "keyword"
+          },
+          "rawScore": {
+            "type": "double"
+          }
+        }
+      },
+      "metadata": {
+        "dynamic": "true",
+        "properties": {
+          "title": {
+            "type": "text"
+          },
+          "name": {
+            "type": "text"
+          },
+          "author": {
+            "type": "text"
+          },
+          "description": {
+            "type": "text"
+          },
+          "producer": {
+            "type": "text"
+          },
+          "publisher": {
+            "type": "text"
+          },
+          "isbn": {
+            "type": "keyword"
+          },
+          "language": {
+            "type": "keyword"
+          },
+          "resourceName": {
+            "type": "keyword"
+          },
+          "keywords": {
+            "type": "text"
+          },
+          "xmpDM:album": {
+            "type": "text"
+          },
+          "xmpDM:albumArtist": {
+            "type": "text"
+          },
+          "xmpDM:artist": {
+            "type": "text"
+          },
+          "xmpDM:composer": {
+            "type": "text"
+          },
+          "Content-Type": {
+            "type": "keyword"
+          },
+          "X-Parsed-By": {
+            "type": "keyword"
+          },
+          "created": {
+            "type": "date",
+            "format": "date_optional_time"
+          },
+          "date": {
+            "type": "date",
+            "format": "date_optional_time"
+          },
+          "modified": {
+            "type": "date",
+            "format": "date_optional_time"
+          }
+        }
+      },
+      "urls": {
+        "type": "keyword"
+      },
+      "size": {
+        "type": "long",
+        "ignore_malformed": true
+      },
+      "references": {
+        "properties": {
+          "name": {
+            "type": "text"
+          },
+          "hash": {
+            "type": "keyword"
+          },
+          "parent_hash": {
+            "type": "keyword"
+          }
         }
       }
     }
   }
+}
 }
 
 # Reindex invalids
