4414fb3cacb58b016672641a055a826a4983decb
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-08-18 12:01:34 +0100

    Update mappings and settings.

--- config/indexes_json.go
+++ config/indexes_json.go
@@ -2,262 +2,261 @@ package config
 
 // Write JSON here to keep correspondence with browser-based settings
 const indexSettingsJSON = `{
-    "index": {
-        "refresh_interval": "15m",
-        "mapping": {
-            "total_fields": {
-                "limit": "8192"
-            }
+  "index": {
+    "refresh_interval": "-1",
+    "mapping": {
+      "total_fields": {
+        "limit": "8192"
+      }
+    },
+    "analysis": {
+      "filter": {
+        "shingle_filter": {
+          "type": "shingle",
+          "min_shingle_size": 5,
+          "max_shingle_size": 5,
+          "output_unigrams": false
         },
-        "analysis": {
-            "filter": {
-                "shingle_filter": {
-                    "type": "shingle",
-                    "min_shingle_size": 5,
-                    "max_shingle_size": 5,
-                    "output_unigrams": false
-                },
-                "minhash_filter": {
-                    "type": "min_hash",
-                    "hash_count": 1,
-                    "bucket_count": 512,
-                    "hash_set_size": 1,
-                    "with_rotation": true
-                }
-            }
-        },
-        "analyzer": {
-            "fingerprint_analyzer": {
-                "tokenizer": "standard",
-                "filter": [
-                  "shingle_filter",
-                  "minhash_filter"
-                ]
-            }
+        "minhash_filter": {
+          "type": "min_hash",
+          "hash_count": 1,
+          "bucket_count": 512,
+          "hash_set_size": 1,
+          "with_rotation": true
         }
-  },
-  "queries": {
-            "cache": {
-                "enabled": "true"
-            }
-        },
-        "number_of_shards" : "5",
-        "number_of_replicas": "1"
+      },
+      "analyzer": {
+        "fingerprint_analyzer": {
+          "tokenizer": "standard",
+          "filter": [
+            "shingle_filter",
+            "minhash_filter"
+          ]
+        }
+      }
     }
+  },
+  "number_of_shards": "20",
+  "number_of_replicas": "0"
 }`
 
 const fileMappingJSON = `{
-    "_doc": {
-        "dynamic": "strict",
-        "dynamic_templates": [
-            {
-                "default_noindex": {
-                    "match": "*",
-                    "mapping": {
-                        "index": false,
-                        "doc_values": false,
-                        "norms": false
-                    }
-                }
-            }
-        ],
-        "properties": {
-            "first-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "last-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "content":  {
-                "type": "text",
-                "term_vector": "with_positions_offsets"
-            },
-            "fingerprint": {
-                "type": "text",
-                "analyzer": "fingerprint_analyzer",
-            },
-            "ipfs_tika_version": {
-                "type": "keyword"
-            },
-            "language": {
-                "properties": {
-                    "confidence": {
-                        "type": "keyword"
-                    },
-                    "language": {
-                        "type": "keyword"
-                    },
-                    "rawScore": {
-                        "type": "double"
-                    }
-                }
-            },
-            "metadata":  {
-                "dynamic":  "true",
-                "properties": {
-                    "title" : {
-                        "type": "text"
-                    },
-                    "name": {
-                        "type": "text"
-                    },
-                    "author": {
-                        "type": "text"
-                    },
-                    "description": {
-                        "type": "text"
-                    },
-                    "producer": {
-                        "type": "text"
-                    },
-                    "publisher": {
-                        "type": "text"
-                    },
-                    "isbn": {
-                        "type": "keyword"
-                    },
-                    "language": {
-                        "type": "keyword"
-                    },
-                    "resourceName": {
-                        "type": "keyword"
-                    },
-                    "keywords": {
-                       "type": "text"
-                    },
-                    "xmpDM:album": {
-                        "type": "text"
-                    },
-                    "xmpDM:albumArtist": {
-                        "type": "text"
-                    },
-                    "xmpDM:artist": {
-                        "type": "text"
-                    },
-                    "xmpDM:composer": {
-                        "type": "text"
-                    },
-                    "Content-Type": {
-                        "type": "keyword"
-                    },
-                    "X-Parsed-By": {
-                        "type": "keyword"
-                    },
-                    "created": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    },
-                    "date": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    },
-                    "modified": {
-                        "type": "date",
-                        "format": "date_optional_time"
-                    }
-                }
-            },
-            "urls": {
-                "type": "keyword"
-            },
-            "size": {
-                "type": "long",
-                "ignore_malformed": true
-            },
-            "references":  {
-                "properties": {
-                    "name": {
-                        "type": "text"
-                    },
-                    "hash": {
-                        "type": "keyword"
-                    },
-                    "parent_hash": {
-                        "type": "keyword"
-                    }
-                }
-            }
+  "dynamic": "strict",
+  "dynamic_templates": [
+    {
+      "default_noindex": {
+        "match": "*",
+        "mapping": {
+          "index": false,
+          "doc_values": false,
+          "norms": false
         }
+      }
     }
+  ],
+  "properties": {
+    "first-seen": {
+      "type": "date",
+      "format": "date_time_no_millis"
+    },
+    "last-seen": {
+      "type": "date",
+      "format": "date_time_no_millis"
+    },
+    "content": {
+      "type": "text",
+      "term_vector": "with_positions_offsets"
+    },
+    "fingerprint": {
+      "type": "text",
+      "analyzer": "fingerprint_analyzer"
+    },
+    "ipfs_tika_version": {
+      "type": "keyword"
+    },
+    "language": {
+      "properties": {
+        "confidence": {
+          "type": "keyword"
+        },
+        "language": {
+          "type": "keyword"
+        },
+        "rawScore": {
+          "type": "double"
+        }
+      }
+    },
+    "metadata": {
+      "dynamic": "true",
+      "properties": {
+        "created": {
+          "type": "date",
+          "format": "date_optional_time",
+          "ignore_malformed": true
+        },
+        "creation-date": {
+          "type": "keyword",
+          "index": false,
+          "doc_values": false
+        },
+        "creationdate": {
+          "type": "keyword",
+          "index": false,
+          "doc_values": false
+        },
+        "title": {
+          "type": "text"
+        },
+        "name": {
+          "type": "text"
+        },
+        "author": {
+          "type": "text"
+        },
+        "description": {
+          "type": "text"
+        },
+        "producer": {
+          "type": "text"
+        },
+        "publisher": {
+          "type": "text"
+        },
+        "isbn": {
+          "type": "keyword"
+        },
+        "language": {
+          "type": "keyword"
+        },
+        "resourceName": {
+          "type": "keyword"
+        },
+        "keywords": {
+          "type": "text"
+        },
+        "xmpDM:album": {
+          "type": "text"
+        },
+        "xmpDM:albumArtist": {
+          "type": "text"
+        },
+        "xmpDM:artist": {
+          "type": "text"
+        },
+        "xmpDM:composer": {
+          "type": "text"
+        },
+        "Content-Type": {
+          "type": "keyword"
+        },
+        "X-Parsed-By": {
+          "type": "keyword"
+        },
+        "date": {
+          "type": "date",
+          "format": "date_optional_time"
+        },
+        "modified": {
+          "type": "date",
+          "format": "date_optional_time"
+        }
+      }
+    },
+    "urls": {
+      "type": "keyword"
+    },
+    "size": {
+      "type": "long",
+      "ignore_malformed": true
+    },
+    "references": {
+      "properties": {
+        "name": {
+          "type": "text"
+        },
+        "hash": {
+          "type": "keyword"
+        },
+        "parent_hash": {
+          "type": "keyword"
+        }
+      }
+    }
+  }
 }`
 
 const dirMappingJSON = `{
-    "_doc": {
-        "dynamic": "strict",
-        "properties": {
-            "first-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "last-seen": {
-                "type": "date",
-                "format": "date_time_no_millis"
-            },
-            "links":  {
-                "dynamic":  true,
-                "properties": {
-                    "Hash": {
-                        "type": "keyword",
-                        "index": true
-                    },
-                    "Name": {
-                        "type": "text"
-                    },
-                    "Size": {
-                       "type": "long",
-                       "ignore_malformed": true
-                    },
-                    "Type": {
-                       "type": "keyword"
-                    }
-                 }
-            },
-            "size": {
-                "type": "long",
-                "ignore_malformed": true
-            },
-            "references":  {
-                "properties": {
-                    "name": {
-                        "type": "text",
-                        "index": true
-                    },
-                    "hash": {
-                        "type": "keyword",
-                        "index": true
-                    },
-                    "parent_hash": {
-                        "type": "keyword",
-                        "index": true
-                    }
-                }
-            }
+  "dynamic": "strict",
+  "properties": {
+    "first-seen": {
+      "type": "date",
+      "format": "date_time_no_millis"
+    },
+    "last-seen": {
+      "type": "date",
+      "format": "date_time_no_millis"
+    },
+    "links": {
+      "dynamic": true,
+      "properties": {
+        "Hash": {
+          "type": "keyword",
+          "index": true
+        },
+        "Name": {
+          "type": "text"
+        },
+        "Size": {
+          "type": "long",
+          "ignore_malformed": true
+        },
+        "Type": {
+          "type": "keyword"
         }
+      }
+    },
+    "size": {
+      "type": "long",
+      "ignore_malformed": true
+    },
+    "references": {
+      "properties": {
+        "name": {
+          "type": "text",
+          "index": true
+        },
+        "hash": {
+          "type": "keyword",
+          "index": true
+        },
+        "parent_hash": {
+          "type": "keyword",
+          "index": true
+        }
+      }
     }
+  }
 }`
 
 const invalidMappingJSON = `{
-    "_doc": {
-        "dynamic_templates": [
-            {
-                "default_noindex": {
-                    "match": "*",
-                    "mapping": {
-                        "index": "no",
-                        "doc_values": false,
-                        "include_in_all": false,
-                        "norms": false
-                    }
-                }
-            }
-        ],
-        "properties": {
-           "error": {
-              "type": "text",
-              "index": false
-           }
+  "dynamic_templates": [
+    {
+      "default_noindex": {
+        "match": "*",
+        "mapping": {
+          "index": "no",
+          "doc_values": false,
+          "include_in_all": false,
+          "norms": false
         }
+      }
     }
+  ],
+  "properties": {
+    "error": {
+      "type": "text",
+      "index": false
+    }
+  }
 }`
